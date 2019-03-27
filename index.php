ce86c1ca4d0c6becadcef763f55bd574bf7ebe37
Thiago Defini Scherer <181710068@senacinformatica.br>
2019-03-27 11:04:28 -0300

    Discord cabeçalho e começo da tela inicial

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Discord - Store</title>
+        <link href="estilo.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+       
+        
+        <div id="cabecalho">
+            
+            <img src="imagens/discord_logo.png" width="140px" id="logo"/>
+            
+            <a href=""><div id="text">Baixar</div></a>
+            <a href=""><div id="text">Nitro</div></a>
+            <a href=""><div id="text">Empregos</div></a>
+            <a href=""><div id="text">Desenvolvedores &#9662</div></a>
+            <a href=""><div id="text">Community &#9662</div></a>
+            <a href=""><div id="text">Suporte &#9662</div></a>
+            
+            
+            <img src="imagens/japa.png" width="25" class="redes" id="japa"/>
+            <span id="entrar" class="redes">Entrar</span>
+            <img src="imagens/instagram_logo.png" width="20" class="redes" id="ig"/>
+            <img src="imagens/facebook_logo.png" width="20" class="redes" id="fb"/>
+            <img src="imagens/twitter_logo.png" width="20" class="redes" id="tw"/>
+            
+            
+        </div>
+            
+            <div class="limpaFloat"></div>
+             
+        <div id="nitro" style="width: 100%; height: 50px;
+            background-color: #36393f;" >
+            <span id="nitro2">Nitro</span>
+        </div>
+            
+             <div class="limpaFloat"></div>
+             
+             <div id="fundo">
+                 <div id="fundo2" >
+                 
+                    <span id="discord_nitro">Discord Nitro</span>
+                     <span id="nitro_text">Support Discord, boost your account with chat perks,
+                         and gain access to over $1000 worth of games and game perks.</span>
+                     <img src="imagens/discord_nitro.png" width="500px"/>
+                 
+                 
+                 </div>
+            </div>     
+             
+        <div style="width: 100%; height: 400px;
+             background-color: #18191c" ></div>
+             
+    </body>
+</html>
